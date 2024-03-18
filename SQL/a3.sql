4f827f0f1c9011120b6b9968ca0f9b2bf35f93d2
Tomas <99683714+SunflowerEdition@users.noreply.github.com>
2024-03-18 16:02:11 -0400

    Update a3.sql

--- SQL/a3.sql
+++ SQL/a3.sql
@@ -1,5 +1,5 @@
 CREATE TABLE students (
-    student_id SERIAL NOT NULL,
+    student_id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
